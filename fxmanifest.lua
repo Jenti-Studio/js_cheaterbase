ddff1c0e5601bef33b248e21a313a936506ede05
Jenti <sabbromeister@gmail.com>
2025-03-30 22:09:31 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+name 'cheater-base'
+description 'Cheater reporting system with screenshots'
+author 'Jenti'
+
+server_scripts {
+    'server.lua'
+}
+
+dependency 'screenshot-basic'
